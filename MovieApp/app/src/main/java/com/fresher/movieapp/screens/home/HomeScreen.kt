f4762fa3ad53e79501ac13c17ec2132cf496ec42
Amruta talekar <amrutatalekar11@gmail.com>
2022-01-24 05:01:48 -0800

    Movie App

---
+++ MovieApp/app/src/main/java/com/fresher/movieapp/screens/home/HomeScreen.kt
@@ -0,0 +1,48 @@
+package com.fresher.movieapp.screens.home
+
+import android.util.Log
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.fresher.movieapp.model.Movie
+import com.fresher.movieapp.model.getMovies
+import com.fresher.movieapp.navigation.MovieScreens
+import com.fresher.movieapp.widgets.MovieRow
+
+@Composable
+fun HomeScreen(navController: NavController){
+
+    Scaffold(topBar = {
+        TopAppBar(backgroundColor = Color.LightGray,
+            elevation = 5.dp) {
+            Text(text = "Movies")
+        }
+    },) {
+        MainContent(navController = navController)
+    }
+}
+
+@Composable
+fun MainContent(
+    navController: NavController,
+    movieList: List<Movie> = getMovies()){
+    Column(modifier = Modifier.padding(12.dp)) {
+        LazyColumn {
+            items(items = movieList) {
+                MovieRow(movie = it){ movie ->
+                    navController.navigate(route = MovieScreens.DetailsScreen.name+ "/$movie")
+                   // Log.d("TAG", "MainContent: $movie")
+                }
+            }
+        }
+    }
+}
